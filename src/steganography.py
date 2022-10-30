76a161c9148d1960681e0411bd28d948b79cb52e
Pierre <pierre.blaess@gmail.fr>
2022-10-30 11:18:25 +0100

    [ADD] Function documentation and config remove dead comment

--- src/steganography.py
+++ src/steganography.py
@@ -2,6 +2,14 @@ import shutil
 from pathlib import Path
 
 def hideTextInImage(text: str, image: str) -> str:
+    """Append the text string into the bottom of the image.
+    It will not modify the given image but create a new one called
+    'hidden.png' at the same position of the original.
+
+    :param text: Plaintext to append at the bottom of the image data.
+    :param image: Path to the given image.
+    :return: The position of the new image.
+    """
     newPath = Path(image).parent.resolve().joinpath('hidden.png')
     shutil.copyfile(image, newPath)
     with open(newPath, 'a') as fp:
