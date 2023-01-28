28fa1fe19b47328c49c729ec07f51caa31f536a4
Pierre <pierre.blaess@gmail.com>
2023-01-28 16:04:22 +0100

    [ADD] passwd file

--- src/app.py
+++ src/app.py
@@ -151,6 +151,41 @@ def admin():
     return render_template('/admin.html', code="__WRONG_BROWSER__", flag=FLAGS['admin-page'], ssh=CONFIG['sshAccess'])
 
 
+@app.route('/.passwd')
+def passwd():
+    return """
+root:x:0:0:root:/root:/bin/ash <br>
+bin:x:1:1:bin:/bin:/sbin/nologin <br>
+daemon:x:2:2:daemon:/sbin:/sbin/nologin <br>
+adm:x:3:4:adm:/var/adm:/sbin/nologin <br>
+lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin <br>
+sync:x:5:0:sync:/sbin:/bin/sync <br>
+shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown <br>
+halt:x:7:0:halt:/sbin:/sbin/halt <br>
+mail:x:8:12:mail:/var/mail:/sbin/nologin <br>
+news:x:9:13:news:/usr/lib/news:/sbin/nologin <br>
+uucp:x:10:14:uucp:/var/spool/uucppublic:/sbin/nologin <br>
+operator:x:11:0:operator:/root:/sbin/nologin <br>
+man:x:13:15:man:/usr/man:/sbin/nologin <br>
+postmaster:x:14:12:postmaster:/var/mail:/sbin/nologin <br>
+cron:x:16:16:cron:/var/spool/cron:/sbin/nologin <br>
+ftp:x:21:21::/var/lib/ftp:/sbin/nologin <br>
+sshd:x:22:22:sshd:/dev/null:/sbin/nologin <br>
+at:x:25:25:at:/var/spool/cron/atjobs:/sbin/nologin <br>
+squid:x:31:31:Squid:/var/cache/squid:/sbin/nologin <br>
+xfs:x:33:33:X Font Server:/etc/X11/fs:/sbin/nologin <br>
+games:x:35:35:games:/usr/games:/sbin/nologin <br>
+cyrus:x:85:12::/usr/cyrus:/sbin/nologin <br>
+vpopmail:x:89:89::/var/vpopmail:/sbin/nologin <br>
+ntp:x:123:123:NTP:/var/empty:/sbin/nologin <br>
+smmsp:x:209:209:smmsp:/var/spool/mqueue:/sbin/nologin <br>
+guest:x:405:100:guest:/dev/null:/sbin/nologin <br>
+nobody:x:65534:65534:nobody:/:/sbin/nologin <br>
+vnstat:x:101:102:vnstat:/var/lib/vnstat:/bin/false <br>
+user:x:1002:1002:,,,:/home/user:/bin/42sh <br>
+"""
+
+
 @app.route('/admin', methods = ['POST'])
 def logout():
     """ POST route to '/admin' page.
